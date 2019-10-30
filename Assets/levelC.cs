9bea80e1e90bd16c74bf45ce39e2302070b0c749
Jorge <jorgeedugutima15@gmail.com>
2019-10-30 01:03:03 -0600

    Transición de escena Versión 1
    Una versión semi-Funcional, está aquí para evitar volverla a crear en caso de error (ya sucedió)

---
+++ Assets/levelC.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class levelC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            FadeToLevel(0);
+        }
+    }
+    public void FadeToLevel (int index){
+        animator.SetTrigger("FadeOut");
+    }
+    
+}
