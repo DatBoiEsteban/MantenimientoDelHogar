b14a6cf4ab2681c72fd61245b73307ae0bd24f25
DatBoiEsteban <esteban1113001@gmail.com>
2019-10-31 13:44:12 -0600

    Con

---
+++ Assets/Scripts/Network/BDConnection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BDConnection : MonoBehaviour
+{
+    public static BDConnection Instance { get; private set; }
+    private const string API_URL = "https://brain-boost-backend.herokuapp.com/api/app_user/auth/";
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
