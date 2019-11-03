c29bb3e49b0c753c0c287fcd9848e07fd8d90f73
Gutima15 <jorgeedugutima15@gmail.com>
2019-11-03 01:43:36 -0600

    Congrats screen con botones funcionales
    Se añade la pantalla de felicitaciones para terminar el juego.

---
+++ Assets/congratsSelectionScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class congratsSelectionScreen : MonoBehaviour
+{
+    public levelC transicion;
+    public void goToSelecion()
+    {
+        transicion.FadeToLevel(1);
+    }
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+}
